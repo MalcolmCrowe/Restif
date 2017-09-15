6e0cce6765b1c0244841611ba5c8ef57a6f9b1ff
MalcolmCrowe <Malcolm.Crowe@uws.ac.uk>
2017-09-15 15:05:57 +0100

    Add files via upload

--- restif/RestifD/RestifD/Restif.cs
+++ restif/RestifD/RestifD/Restif.cs
@@ -11,12 +11,15 @@ namespace RestifD
         HttpListenerContext context;
         static string host = "localhost";
         MySqlConnection conn = null;
+        string origin = null;
         static void Main(string[] args)
         {
             if (args.Length > 0)
                 host = args[0];
             var listener = new HttpListener();
             listener.Prefixes.Add("http://" + host + ":8078/");
+            listener.Prefixes.Add("https://"+host+":4437/");
+            listener.Realm = "MySQL server on " + host;
             listener.AuthenticationSchemes = AuthenticationSchemes.Basic;
             listener.Start();
             for (;;)
@@ -37,26 +40,39 @@ namespace RestifD
         void Run()
         {
             Task.Run(() =>
-          {
+             {
                 var meth = context.Request.HttpMethod;
                 var path = context.Request.Url.Segments;
+                origin = context.Request.Headers["Origin"];
                 var h = context.Request.Headers["Authorization"];
                 var s = Encoding.UTF8.GetString(Convert.FromBase64String(h.Substring(6))).Split(':');
                 string db = get(path, 1);
                 string tb = get(path, 2);
                 string wh = get(path, 3);
+                var page = db != null && (db.EndsWith(".htm")||db.EndsWith(".js"));
                 var connstring = "server=" + host + ";uid="+s[0]+";password="+s[1];
                 if (db != null)
                     connstring += ";database=" + db;
                 string mess = null;
                 try
                 {
-                    conn = new MySqlConnection(connstring);
-                    conn.Open();
+                    if (!page)
+                        conn = new MySqlConnection(connstring);
+                    conn?.Open();
                     switch (meth)
                     {
+                        case "OPTIONS":
+                           SendCORS();
+                           return;
                         case "GET":
-                            mess = (tb==null)?Get1(db):Get2(tb,wh);
+                          if (page)
+                          {
+                              var rdr = new StreamReader(db);
+                              mess = rdr.ReadToEnd();
+                              rdr.Close();
+                          }
+                          else
+                              mess = (tb == null) ? Get1(db) : Get2(tb, wh);
                             break;
                         case "POST":
                             var pd = Receive();
@@ -107,14 +123,13 @@ namespace RestifD
             //  0: fieldstart 1: fieldend 2: liststart 3: listmid 4: listend 
             //  5: rowstart 6: rowmid 7: rowend  
             var fmt = new string[] { "", "=", "", "\n", "", "", ",", ";" };
-
             switch (acc)
             {
                 case "application/json":
                     fmt = new string[] { "\"", "\": ", "[", ",\n", "]", "{", ", ", "}" };
                     break;
                 case "text/html":
-                    fmt = new string[] { "", "", "</tr>", "", "</table>", "<tr>","", "</tr>" };
+                    fmt = new string[] { "", "", "<table>", "", "</table>", "<tr>","", "</tr>" };
                     break;
                 case "application/xml":
                     fmt = new string[] { "<", ">", "<root>", "", "</root>", "<" + tb + ">", "", "</" + tb + ">" };
@@ -127,9 +142,10 @@ namespace RestifD
             {
                 if (acc=="text/html" && pos++ ==0) // generate headers row
                 {
-                    r.Append("<table border><tr>");
+                    r.Append("<tr>");
                     for (var i = 0; i < rdr.VisibleFieldCount; i++)
                         r.Append("<th>" + rdr.GetName(i) + "</th>");
+                    r.Append("</tr>");
                 }
                 var c = "";
                 r.Append(m);
@@ -139,7 +155,7 @@ namespace RestifD
                 {
                     var fn = rdr.GetName(i); // tag or field name
                     if (acc == "text/html") // html headers have already been done
-                        r.Append("<td>"+rdr[i].ToString()+"</td>");
+                        r.Append("<td>"+Format1(rdr[i])+"</td>");
                     else
                         r.Append(c + fmt[0] + fn  + fmt[1] +Format(rdr[i]));
                     if (acc == "application/xml") // xml need end tags
@@ -210,10 +226,21 @@ namespace RestifD
             if (ob is DateTime)
             {
                 var dt = (DateTime)ob;
-                return "'"+dt.ToString("yyyy-MM-dd")+"'";
+                return dt.ToString("yyyy-MM-dd'T'HH:mm'Z'"); // ISO8601
             }
             if (ob is string)
-                return "'" + ob.ToString() + "'";
+                return "'" + ob.ToString().Replace("'","''") + "'";
+            return ob.ToString();
+        }
+        string Format1(object ob)
+        {
+            if (ob is DBNull)
+                return "null";
+            if (ob is DateTime)
+            {
+                var dt = (DateTime)ob;
+                return dt.ToString("yyyy-MM-dd'T'HH:mm'Z'"); // ISO8601
+            }
             return ob.ToString();
         }
         string Receive()
@@ -227,12 +254,27 @@ namespace RestifD
             rdr.Close();
             return rs;
         }
+        void SendCORS()
+        {
+            var h = context.Request.Headers["Access-Control-Request-Headers"];
+            var c = context.Response;
+            c.StatusCode = 200;
+            c.StatusDescription = "OK";
+            c.AddHeader("Acess-Control-Allow-Origin", origin);
+            c.AddHeader("Acess-Control-Allow-Headers", h);
+            c.AddHeader("Acess-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            c.AddHeader("Acess-Control-Max-Age", "86400");
+            c.ContentLength64 = 0;
+            c.OutputStream.Close();
+        }
         void Send(int status,string mess)
         {
             var b = Encoding.UTF8.GetBytes(mess);
             var c = context.Response;
             c.StatusCode = status;
             c.StatusDescription = (status == 200) ? "OK" : "ERROR";
+            if (origin!=null)
+                c.AddHeader("Acess-Control-Allow-Origin", origin);
             c.AddHeader("Cache-control", "no-store");
             c.AddHeader("Expires", "-1");
             c.AddHeader("Pragma", "no-cache");
