95643cd799c27f5bf2c81c6dcc557e69ca9f16a8
MalcolmCrowe <Malcolm.Crowe@uws.ac.uk>
2018-06-07 09:53:49 +0100

    Add files via upload

---
+++ RestifS/RestifS/Document.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestifD
+{
+    /// <summary>
+    /// The Document class here represents JavaScript style objects.
+    /// The constructor and ToString() support JSon serialisation.
+    /// There are methods using reflection to create a Document representing
+    /// any CLI object of a known type, 
+    /// and for extracting objects from a Document given a path of fieldnames.
+    /// See also DocArray,
+    /// </summary>
+    public class Document : DocBase
+    {
+        public List<KeyValuePair<string, object>> fields = new List<KeyValuePair<string, object>>();
+        public Document()
+        { }
+        public Document(string s)
+        {
+            if (s == null)
+                return;
+            s = s.Trim();
+            int n = s.Length;
+            if (n == 0 || s[0] != '{')
+                throw new DocumentException("{ expected");
+            var i = Fields(s, 1, n);
+            if (i != n)
+                throw new DocumentException("unparsed input at " + (i - 1));
+        }
+        public object this[string k]
+        {
+            get
+            {
+                foreach (var p in fields)
+                    if (p.Key == k)
+                        return p.Value;
+                return null;
+            }
+        }
+        public bool Contains(string k)
+        {
+            foreach (var p in fields)
+                if (p.Key == k)
+                    return true;
+            return false;
+        }
+        private enum ParseState { StartKey, Key, Colon, StartValue, Comma }
+        /// <summary>
+        /// Parse the contents of {} 
+        /// </summary>
+        /// <param name="s">the string</param>
+        /// <param name="i">the start of the fields</param>
+        /// <param name="n">the end of the string</param>
+        /// <returns>the position just after the }</returns>
+        internal int Fields(string s, int i, int n)
+        {
+            ParseState state = ParseState.StartKey;
+            StringBuilder kb = null;
+            while (i < n)
+            {
+                var c = s[i++];
+                switch (state)
+                {
+                    case ParseState.StartKey:
+                        kb = new StringBuilder();
+                        if (Char.IsWhiteSpace(c))
+                            continue;
+                        if (c == '}' && fields.Count == 0)
+                            return i;
+                        if (c != '"')
+                            throw new DocumentException("Expected \" at " + (i - 1));
+                        state = ParseState.Key;
+                        continue;
+                    case ParseState.Key:
+                        if (c == '"')
+                        {
+                            state = ParseState.Colon;
+                            continue;
+                        }
+                        if (c == '\\')
+                            c = GetEscape(s, n, ref i);
+                        kb.Append(c);
+                        continue;
+                    case ParseState.Colon:
+                        if (Char.IsWhiteSpace(c))
+                            continue;
+                        if (c != ':')
+                            throw new DocumentException("Expected : at " + (i - 1));
+                        state = ParseState.StartValue;
+                        continue;
+                    case ParseState.StartValue:
+                        if (Char.IsWhiteSpace(c))
+                            continue;
+                        fields.Add(new KeyValuePair<string, object>
+                                (kb.ToString(), GetValue(s, n, ref i)));
+                        state = ParseState.Comma;
+                        continue;
+                    case ParseState.Comma:
+                        if (Char.IsWhiteSpace(c))
+                            continue;
+                        if (c == '}')
+                            return i;
+                        if (c != ',')
+                            throw new DocumentException("Expected , at " + (i - 1));
+                        state = ParseState.StartKey;
+                        continue;
+                }
+            }
+            throw new DocumentException("Incomplete syntax at " + (i - 1));
+        }
+        public static void Field(object v, StringBuilder sb)
+        {
+            if (v == null || v is DBNull)
+                sb.Append("null");
+            else if (v is string)
+            {
+                v = ((string)v).Replace("'", "''");
+                sb.Append('\'');
+                sb.Append(v);
+                sb.Append('\'');
+            }
+            else if (v is IEnumerable)
+            {
+                var comma = "";
+                sb.Append("[");
+                foreach (var a in (IEnumerable)v)
+                {
+                    sb.Append(comma);
+                    comma = ",";
+                    if (a is Document)
+                        sb.Append(((Document)a).ToString());
+                    else if (a is DocArray)
+                        sb.Append(((DocArray)a).ToString());
+                    else if (a is DateTime)
+                        sb.Append((((DateTime)a).Ticks*1.0D/TimeSpan.TicksPerSecond).ToString());
+                    else if (a is string)
+                        sb.Append("'" + a + "'");
+                    else
+                        sb.Append(a.ToString());
+                }
+                sb.Append("]");
+            }
+            else
+                sb.Append(v.ToString());
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder("{");
+            var comma = "";
+            foreach (var f in fields)
+            {
+                sb.Append(comma); comma = ", ";
+                sb.Append('"');
+                sb.Append(f.Key);
+                sb.Append("\": ");
+                Field(f.Value, sb);
+            }
+            sb.Append("}");
+            return sb.ToString();
+        }
+        internal static int GetLength(byte[] b, int off)
+        {
+            return b[off] + (b[off + 1] << 8) + (b[off + 2] << 16) + (b[off + 3] << 24);
+        }
+
+        internal static void Format(Document e, StringBuilder cls, StringBuilder vls)
+        {
+            var comma = "";
+            foreach (var f in e.fields)
+            {
+                cls?.Append(comma);
+                vls.Append(comma);
+                comma = ", ";
+                cls?.Append(f.Key);
+                Field(f.Value, vls);
+            }
+        }
+    }
+    public class DocArray : DocBase
+    {
+        public List<object> items = new List<object>();
+        public DocArray() { }
+        public DocArray(string s)
+        {
+            if (s == null)
+                return;
+            s = s.Trim();
+            int n = s.Length;
+            if (n <= 2 || s[0] != '[' || s[n - 1] != ']')
+                throw new DocumentException("[..] expected");
+            int i = Items(s, 1, n);
+            if (i != n)
+                throw new DocumentException("bad DocArray format");
+        }
+        private enum ParseState { StartValue, Comma }
+        internal int Items(string s, int i, int n)
+        {
+            var state = ParseState.StartValue;
+            while (i < n)
+            {
+                var c = s[i++];
+                if (Char.IsWhiteSpace(c))
+                    continue;
+                if (c == ']' && items.Count == 0)
+                    break;
+                switch (state)
+                {
+                    case ParseState.StartValue:
+                        items.Add(GetValue(s, n, ref i));
+                        state = ParseState.Comma;
+                        continue;
+                    case ParseState.Comma:
+                        if (c == ']')
+                            return i;
+                        if (c != ',')
+                            throw new DocumentException(", expected");
+                        state = ParseState.StartValue;
+                        continue;
+                }
+            }
+            return i;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder("[");
+            var comma = "";
+            foreach (var e in items)
+            {
+                sb.Append(comma);
+                sb.Append(e.ToString());
+                comma = ",";
+            }
+            sb.Append("]");
+            return sb.ToString();
+        }
+   }
+    public class DocumentException : Exception
+    {
+        public DocumentException(string msg) : base(msg) { }
+    }
+    public class DocBase
+    {
+        protected DocBase() { }
+        protected object GetValue(string s, int n, ref int i)
+        {
+            if (i < n)
+            {
+                var c = s[i - 1];
+                if (c == '"' || c=='\'')
+                    return GetString(s, n, c, ref i);
+                if (c == '{')
+                {
+                    var d = new Document();
+                    i = d.Fields(s, i, n);
+                    return d;
+                }
+                if (c == '[')
+                {
+                    var d = new DocArray();
+                    i = d.Items(s, i, n);
+                    return d;
+                }
+                if (i + 3 < n && s.Substring(i - 1, 4) == "true")
+                {
+                    i += 3;
+                    return true;
+                }
+                if (i + 4 < n && s.Substring(i - 1, 5) == "false")
+                {
+                    i += 4;
+                    return false;
+                }
+                if (i + 3 < n && s.Substring(i - 1, 4) == "null")
+                {
+                    i += 3;
+                    return null;
+                }
+                var sg = c == '-';
+                if (sg && i < n)
+                    c = s[i++];
+                var whole = 0L;
+                if (Char.IsDigit(c))
+                {
+                    i--;
+                    whole = GetHex(s, n, ref i);
+                    while (i < n && Char.IsDigit(s[i]))
+                        whole = whole * 10 + GetHex(s, n, ref i);
+                }
+                else
+                    goto bad;
+                if (i >= n || (s[i] != '.' && s[i] != 'e' && s[i] != 'E'))
+                    return sg ? -whole : whole;
+                int scale = 0;
+                if (s[i] == '.')
+                {
+                    if (++i >= n || !Char.IsDigit(s[i]))
+                        throw new DocumentException("decimal part expected");
+                    while (i < n && Char.IsDigit(s[i]))
+                    {
+                        whole = whole * 10 + GetHex(s, n, ref i);
+                        scale++;
+                    }
+                }
+                if (i >= n || (s[i] != 'e' && s[i] != 'E'))
+                {
+                    var m = (decimal)whole;
+                    while (scale-- > 0)
+                        m /= 10M;
+                    return sg ? -m : m;
+                }
+                if (++i >= n)
+                    throw new DocumentException("exponent part expected");
+                var esg = s[i] == '-';
+                if ((s[i] == '-' || s[i] == '+') && (++i >= n || !Char.IsDigit(s[i])))
+                    throw new DocumentException("exponent part expected");
+                var exp = 0;
+                while (i < n && Char.IsDigit(s[i]))
+                    exp = exp * 10 + GetHex(s, n, ref i);
+                if (esg)
+                    exp = -exp;
+                var dr = whole * Math.Pow(10.0, exp - scale);
+                return sg ? -dr : dr;
+            }
+            bad:
+            throw new DocumentException("Value expected at " + (i - 1));
+        }
+        protected string GetString(string s, int n, char q, ref int i)
+        {
+            var sb = new StringBuilder();
+            while (i < n)
+            {
+                var c = s[i++];
+                if (c == q)
+                    return sb.ToString();
+                if (c == '\\')
+                    c = GetEscape(s, n, ref i);
+                sb.Append(c);
+            }
+            throw new DocumentException("Non-terminated string at " + (i - 1));
+        }
+        protected char GetEscape(string s, int n, ref int i)
+        {
+            if (i < n)
+            {
+                var c = s[i++];
+                switch (c)
+                {
+                    case '"': return c;
+                    case '\\': return c;
+                    case '/': return c;
+                    case 'b': return '\b';
+                    case 'f': return '\f';
+                    case 'n': return '\n';
+                    case 'r': return '\r';
+                    case 't': return '\t';
+                    case 'u':
+                        {
+                            int v = 0;
+                            for (int j = 0; j < 4; i++)
+                                v = (v << 4) + GetHex(s, n, ref i);
+                            return (char)v;
+                        }
+                    case 'U': goto case 'u';
+                }
+            }
+            throw new DocumentException("Illegal escape");
+        }
+        internal static int GetHex(string s, int n, ref int i)
+        {
+            if (i < n)
+            {
+                switch (s[i++])
+                {
+                    case '0': return 0;
+                    case '1': return 1;
+                    case '2': return 2;
+                    case '3': return 3;
+                    case '4': return 4;
+                    case '5': return 5;
+                    case '6': return 6;
+                    case '7': return 7;
+                    case '8': return 8;
+                    case '9': return 9;
+                    case 'a': return 10;
+                    case 'b': return 11;
+                    case 'c': return 12;
+                    case 'd': return 13;
+                    case 'e': return 14;
+                    case 'f': return 15;
+                    case 'A': return 10;
+                    case 'B': return 11;
+                    case 'C': return 12;
+                    case 'D': return 13;
+                    case 'E': return 14;
+                    case 'F': return 15;
+                }
+            }
+            throw new DocumentException("Hex digit expected at " + (i - 1));
+        }
+
+    }
+
+}
---
+++ RestifS/RestifS/Restif.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+/// <summary>
+/// SQLSERVER Restif interface. Use SQLServer authentication.
+/// </summary>
+namespace RestifD
+{
+    class Restif
+    {
+        HttpListenerContext context;
+        static string host = "localhost";
+        SqlConnection conn = null;
+        string origin = null;
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                host = args[0];
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://" + host + ":8178/");
+            listener.Prefixes.Add("https://"+host+":4438/");
+            listener.Realm = "SqlServer on " + host;
+            listener.AuthenticationSchemes = AuthenticationSchemes.Basic;
+            listener.Start();
+            for (;;)
+                try
+                {
+                    new Restif { context = listener.GetContext() }.Run();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+        }
+        string get(string[] p, int i)
+        {
+            return (i < p.Length) ? (p[i] != "") ? p[i].Trim('/') : null : null;
+        }
+        void Run()
+        {
+            Task.Run(() =>
+             {
+                var meth = context.Request.HttpMethod;
+                var path = context.Request.Url.Segments;
+                origin = context.Request.Headers["Origin"];
+                var h = context.Request.Headers["Authorization"];
+                var s = Encoding.UTF8.GetString(Convert.FromBase64String(h.Substring(6))).Split(':');
+                string db = get(path, 1);
+                string tb = get(path, 2);
+                string wh = get(path, 3);
+                var page = db != null && (db.EndsWith(".htm")||db.EndsWith(".js"));
+                var connstring = "Data Source=" + host + ";Persist Security Info=True;User ID=" + s[0]+";Password="+s[1];
+                if (db != null)
+                    connstring += ";Initial Catalog=" + db;
+                string mess = null;
+                try
+                {
+                    if (!page)
+                        conn = new SqlConnection(connstring);
+                    conn?.Open();
+                    switch (meth)
+                    {
+                        case "OPTIONS":
+                           SendCORS();
+                           return;
+                        case "GET":
+                          if (page)
+                          {
+                              var rdr = new StreamReader(db);
+                              mess = rdr.ReadToEnd();
+                              rdr.Close();
+                          }
+                          else
+                              mess = (tb == null) ? Get1(db) : Get2(tb, wh);
+                            break;
+                        case "POST":
+                            var pd = Receive();
+                            if (tb != null)
+                                mess = Post2(tb, new DocArray(pd));
+                            else if (db != null)
+                                mess = Post1(pd);
+                            else
+                                throw new Exception("not supported");
+                            break;
+                        case "PUT":
+                            if (db == null || tb == null || wh == null)
+                                throw new Exception("not supported");
+                            mess = Put(tb,wh,new Document(Receive()));
+                            break;
+                        case "DELETE":
+                            if (db == null || tb == null)
+                                throw new Exception("not supported");
+                            mess = Delete(tb,wh);
+                            break;
+                    }
+                    Send(200, mess);
+                } catch (Exception e)
+                {
+                    Send(403, e.Message);
+                     Console.WriteLine("--> "+e.Message);
+                }
+                conn?.Close();
+            });
+        }
+        string Get1(string db)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = (db==null)?"show databases":"show tables";
+            var rdr = cmd.ExecuteReader();
+            var r = "";
+            while (rdr.Read())
+                r += " " + rdr[0].ToString();
+            return r;
+        }
+        string Get2(string tb, string wh)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "select * from " + tb;
+            if (wh != null)
+                cmd.CommandText += " where " + wh;
+            var rdr = cmd.ExecuteReader();
+             var acc = context.Request.Headers["Accept"];
+            //  0: fieldstart 1: fieldend 2: liststart 3: listmid 4: listend 
+            //  5: rowstart 6: rowmid 7: rowend  
+            var fmt = new string[] { "", "=", "", "\n", "", "", ",", ";" };
+            switch (acc)
+            {
+                case "application/json":
+                    fmt = new string[] { "\"", "\": ", "[", ",\n", "]", "{", ", ", "}" };
+                    break;
+                case "text/html":
+                    fmt = new string[] { "", "", "<table>", "", "</table>", "<tr>","", "</tr>" };
+                    break;
+                case "application/xml":
+                    fmt = new string[] { "<", ">", "<root>", "", "</root>", "<" + tb + ">", "", "</" + tb + ">" };
+                    break;
+            }
+            var r = new StringBuilder(fmt[2]);
+            var pos = 0;
+            var m = "";
+            while (rdr.Read())
+            {
+                if (acc=="text/html" && pos++ ==0) // generate headers row
+                {
+                    r.Append("<tr>");
+                    for (var i = 0; i < rdr.VisibleFieldCount; i++)
+                        r.Append("<th>" + rdr.GetName(i) + "</th>");
+                    r.Append("</tr>");
+                }
+                var c = "";
+                r.Append(m);
+                m = fmt[3];
+                r.Append(fmt[5]);
+                for (var i = 0; i < rdr.VisibleFieldCount; i++)
+                {
+                    var fn = rdr.GetName(i); // tag or field name
+                    if (acc == "text/html") // html headers have already been done
+                        r.Append("<td>"+Format1(rdr[i])+"</td>");
+                    else
+                        r.Append(c + fmt[0] + fn  + fmt[1] +Format(rdr[i]));
+                    if (acc == "application/xml") // xml need end tags
+                        r.Append("</" + fn + ">");
+                    c = fmt[6];
+                }
+                r.Append(fmt[7]);
+            }
+            return r + fmt[4];
+        }
+        /// <summary>
+        /// This method allows for transactional operation: 
+        /// the data is generally a sequence of SQL statements,
+        /// to be performed in a transaction.
+        /// We also allow queries: in which case the result sets (Json arrays) are simply concatenated.
+        /// The current implementation assumes everyone agrees to use SQL as in ISO 9075:2016 ;)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        string Post1(string data)
+        {
+            var tr = conn.BeginTransaction(System.Data.IsolationLevel.Serializable);
+            var ret = "";
+            foreach (var s in data.Split(';'))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.Transaction = tr;
+                var cm = s.Trim();
+                if (cm == "")
+                    continue;
+                cmd.CommandText = cm;
+                cm = cm.ToUpper();
+                Console.WriteLine(cm);
+                if (cm.StartsWith("SELECT") || cm.StartsWith("TABLE"))
+                {
+                    var rdr = cmd.ExecuteReader();
+                    var docar = new DocArray();
+                    while (rdr.Read())
+                    {
+                        var doc = new Document();
+                        for (var i = 0; i < rdr.VisibleFieldCount; i++)
+                            doc.fields.Add(new KeyValuePair<string, object>(rdr.GetName(i), rdr.GetValue(i)));
+                        docar.items.Add(doc);
+                    }
+                    rdr.Close();
+                    ret += docar.ToString();
+                }
+                else
+                    cmd.ExecuteNonQuery();
+            }
+            tr.Commit();
+            if (ret == "")
+                ret = "OK";
+            return ret;
+        }
+        string Post2(string tb,DocArray d)
+        {
+            var cls = new StringBuilder("insert into " + tb + "(");
+            var vls = new StringBuilder(") values ");
+            var cm = "";
+            foreach (Document e in d.items)
+            {
+                vls.Append(cm);
+                vls.Append("(");
+                Document.Format(e, (cm == "") ? cls : null, vls);
+                vls.Append(")");
+                cm = ",";
+            }
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = cls.ToString() + vls.ToString();
+            cmd.ExecuteNonQuery();
+            return "OK";
+        }
+        string Put(string tb,string wh,Document d)
+        {
+            var cmt = "update " + tb + " set ";
+            var cm = "";
+            foreach (var e in d.fields)
+            {
+                cmt += cm + e.Key + "=" + Format(e.Value);
+                cm = ",";
+            }
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = cmt+" where "+wh;
+            cmd.ExecuteNonQuery();
+            return "OK";
+        }
+        string Delete(string tb,string wh)
+        {
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "delete from " + tb;
+            if (wh != null)
+                cmd.CommandText += " where " + wh;
+            return "" + cmd.ExecuteNonQuery() + " rows deleted";
+        }
+        string Format(object ob)
+        {
+            if (ob is DBNull)
+                return "null";
+            if (ob is DateTime)
+                return ((DateTime)ob).ToString("yyyy-MM-dd'T'HH:mm'Z'"); // ISO8601
+            if (ob is string)
+                return "'" + ob.ToString().Replace("'","''") + "'";
+            if (ob is Document)
+            {
+                var sb = new StringBuilder();
+                var cm = "";
+                var d = (Document)ob;
+                foreach(var x in d.fields)
+                {
+                    sb.Append(cm); cm = ",";
+                    Document.Field(x, sb);
+                }
+                return sb.ToString();
+            }
+            return ob.ToString();
+        }
+        string Format1(object ob)
+        {
+            if (ob is DBNull)
+                return "null";
+            if (ob is DateTime)
+                return ((DateTime)ob).ToString("yyyy-MM-dd'T'HH:mm'Z'"); // ISO8601
+            return ob.ToString();
+        }
+        string Receive()
+        {
+            var r = context.Request;
+            if (!r.HasEntityBody)
+                return "No data??";
+            var rdr = new StreamReader(r.InputStream, r.ContentEncoding);
+            var rs = rdr.ReadToEnd();
+            r.InputStream.Close();
+            rdr.Close();
+            return rs;
+        }
+        void SendCORS()
+        {
+            var h = context.Request.Headers["Access-Control-Request-Headers"];
+            var c = context.Response;
+            c.StatusCode = 200;
+            c.StatusDescription = "OK";
+            c.AddHeader("Access-Control-Allow-Origin", origin);
+            c.AddHeader("Access-Control-Allow-Headers", h);
+            c.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            c.AddHeader("Access-Control-Max-Age", "86400");
+            c.ContentLength64 = 0;
+            c.OutputStream.Close();
+        }
+        void Send(int status,string mess)
+        {
+            var b = Encoding.UTF8.GetBytes(mess);
+            var c = context.Response;
+            c.StatusCode = status;
+            c.StatusDescription = (status == 200) ? "OK" : "ERROR";
+            if (origin!=null)
+                c.AddHeader("Access-Control-Allow-Origin", origin);
+            c.AddHeader("Cache-control", "no-store");
+            c.AddHeader("Expires", "-1");
+            c.AddHeader("Pragma", "no-cache");
+            if (mess != null && mess.Length > 0 && mess[0] == '[')
+                c.AddHeader("Content-Type", "application/json");
+            c.ContentLength64 = b.Length;
+            var st = c.OutputStream;
+            st.Write(b, 0, b.Length);
+            st.Close();
+        }
+    }
+}
